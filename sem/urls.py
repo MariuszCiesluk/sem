7abbbe9d29efacb796e910e281f4b1876678266a
mariusz <budkagracza@gmail.com>
2016-01-05 18:25:57 +0100

    clean main urls

--- sem/urls.py
+++ sem/urls.py
@@ -1,17 +1,11 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'sem.views.home', name='home'),
-    # url(r'^sem/', include('sem.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
-)
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^tasks/', include('core.rest_urls')),
+                       url(r'^', include('core.urls')),
+                       )
