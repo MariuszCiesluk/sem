dd78f3bd9e496c0ae3870dd416907232565a44eb
Mariusz <budkagracza@gmail.com>
2016-01-13 00:00:02 +0100

    versioning rest api

--- core/rest_urls.py
+++ core/rest_urls.py
@@ -3,11 +3,11 @@ from django.conf.urls import *
 from core.rest_views import TaskRestListView, TaskRestView, TaskListElementRestListView, TaskListElementRestView
 
 urlpatterns = (
-    url(r'^list/$', TaskRestListView.as_view(), name='tasks-list'),
-    url(r'^list/(?P<pk>[0-9]+)/$', TaskRestView.as_view(), name='edit-task'),
+    url(r'^v1/list/$', TaskRestListView.as_view(), name='tasks-list'),
+    url(r'^v1/list/(?P<pk>[0-9]+)/$', TaskRestView.as_view(), name='edit-task'),
 
-    url(r'^list-elements/(?P<task_pk>[0-9]+)/$', TaskListElementRestListView.as_view(), name='task-element-list'),
-    url(r'^list-elements/(?P<task_pk>[0-9]+)/(?P<pk>[0-9]+)/$', TaskListElementRestView.as_view(), name='edit-element-task'),
+    url(r'^v1/list-elements/(?P<task_pk>[0-9]+)/$', TaskListElementRestListView.as_view(), name='task-element-list'),
+    url(r'^v1/list-elements/(?P<task_pk>[0-9]+)/(?P<pk>[0-9]+)/$', TaskListElementRestView.as_view(), name='edit-element-task'),
 )
 
 # from rest_framework.authtoken import views
